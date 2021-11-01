43d3e9852949e60f234d6f751324fc1c04abf502
StiveEsau <stiveroncal@gmail.com>
2021-10-31 22:46:25 -0500

    Creacion de id show-menu junto a class menu que trabaja con los enlaces, trabajar con clases imaginaria creado en css, poniendo animacion de retraso para barra de lateral

--- js/script.js
+++ js/script.js
@@ -7,4 +7,8 @@ document.getElementById("icon-menu").addEventListener("click", mostrar_menu);
 function mostrar_menu(){
     // Relacon de la caja que contiene la portada,article y footer que esta como un id 
     document.getElementById("move-content").classList.toggle('move-container-all');
+
+    // Trabajando con Class imaginaria de Css de show-lateral y crecion de id en caja de menu de los enlazes
+    // Proceso es caja de enlaces donde esta un id y luego el toggle con la clase imaginaria  de css
+    document.getElementById("show-menu").classList.toggle('show-lateral')
 }
